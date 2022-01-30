90cdd24f93f21b12ed23c28e55a5e148cb76a9fa
YOSHITHA RATHNAYAKE <Y.S.B.Rathnayake2008@gmail.com>
2022-01-30 15:20:48 +0530

    Update App.js

--- Assets/App.js
+++ Assets/App.js
@@ -1,1 +1,123 @@
+UserName = prompt("Hi, I am Yoshitha Rathnayake. What is your name?");
+    alert("Hello, " + UserName + " Welcome to my Quiz that I made using JavaScript (Language).");
 
+
+    function clickMe() {
+        document.getElementById('If').style.display = 'block';
+        textAlign = 'center';
+    }
+
+    function doubleClick() {
+        document.getElementById('If').style.display = 'none';
+        textAlign = 'center';
+    }
+
+    function backgroundColorWhite() {
+        document.getElementById('body').style.backgroundColor = 'white';
+        alert('You select White color for your background!');
+        console.log('You select white color for your background!');
+    }
+
+    function backgroundColorRed() {
+        document.getElementById('body').style.backgroundColor = 'red';
+        alert('You select Red color for your background!');
+        console.log('You select Red color for your background!');
+    }
+
+    function backgroundColorGreen() {
+        document.getElementById('body').style.backgroundColor = '#00ff00';
+        alert('You select Green color for your background!');
+        console.log('You select Green color for your background!');
+    }
+
+    function backgroundColorBlue() {
+        document.getElementById('body').style.backgroundColor = 'aqua';
+        alert('You select Blue color for your background!');
+        console.log('You select Blue color for your background!');
+    }
+
+    function backgroundColorBlack() {
+        document.getElementById('body').style.backgroundColor = 'black';
+        alert('You select Black color for your background!');
+        console.log('You select Black color for your background!');
+    }
+
+    function clickMe2() {
+        document.getElementById('My').style.display = 'block';
+        textAlign = 'center';
+    }
+
+    function doubleClick2() {
+        document.getElementById('My').style.display = 'none';
+        textAlign = 'center';
+    }
+
+    function fontColorWhite() {
+        document.getElementById('body').style.color = 'white';
+        alert('You select White color for above heading!');
+        console.log('You select white color for above heading!');
+    }
+
+    function fontColorRed() {
+        document.getElementById('body').style.color = 'red';
+        alert('You select Red color for above heading!');
+        console.log('You select Red color for above heading!');
+    }
+
+    function fontColorGreen() {
+        document.getElementById('body').style.color = '#00ff00';
+        alert('You select Green color for above heading!');
+        console.log('You select Green color for above heading!');
+    }
+
+    function fontColorBlue() {
+        document.getElementById('body').style.color = 'aqua';
+        alert('You select Blue color for above heading!');
+        console.log('You select Blue color for above heading!');
+    }
+
+    function fontColorBlack() {
+        document.getElementById('body').style.color = 'black';
+        alert('You select Black color for above heading!');
+        console.log('You select Black color for above heading!');
+    }
+
+    function clickMe3() {
+        document.getElementById('Me').style.display = 'block';
+        textAlign = 'center';
+    }
+
+    function doubleClick3() {
+        document.getElementById('Me').style.display = 'none';
+        textAlign = 'center';
+    }
+
+    function fontSize10px() {
+        document.getElementById('id').style.fontSize = '10px';
+        alert('You select 10px for above heading!');
+        console.log('You select 10px for above heading!');
+    }
+
+    function fontSize30px() {
+        document.getElementById('id').style.fontSize = '30px';
+        alert('You select 30px for above heading!');
+        console.log('You select 30px for above heading!');
+    }
+
+    function fontSize50px() {
+        document.getElementById('id').style.fontSize = '50px';
+        alert('You select 50px for above heading!');
+        console.log('You select 50px for above heading!');
+    }
+
+    function fontSize80px() {
+        document.getElementById('id').style.fontSize = '80px';
+        alert('You select 80px for above heading!');
+        console.log('You select 80px for above heading!');
+    }
+
+    function fontSize100px() {
+        document.getElementById('id').style.fontSize = '100px';
+        alert('You select 100px for above heading!');
+        console.log('You select 100px for above heading!');
+    }

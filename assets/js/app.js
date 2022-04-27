508171d322967f15bb8412db5bc33800b5a55fdb
YOSHITHA RATHNAYAKE <Y.S.B.Rathnayake2008@gmail.com>
2022-04-27 16:37:49 +0530

    Update and rename assets/app.js to assets/js/app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
+// User Name
+
 UserName = prompt("Hi, I am Yoshitha Rathnayake. What is your name?");
     alert("Hello, " + UserName + " Welcome to my Quiz that I made using JavaScript (Language).");
 
+
+    // Buttons Script
 
     function clickMe() {
         document.getElementById('If').style.display = 'block';
